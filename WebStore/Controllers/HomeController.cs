c5e1408c077e10b285ebfe82cc42ed0bec9533a9
German322VDK <germean322@gmail.com>
2021-01-16 19:44:22 +1000

    Визуализация списка сотрудников

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -3,15 +3,25 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStore.Models;
 
 namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
+        public static readonly List<Employee> employees = new()
+        {
+            new Employee { Id=1, LastName = "Петров", FirstName = "Иван", Patronymic = "Александрович",  Age = 40},
+            new Employee { Id = 2, LastName = "Иванов", FirstName = "Александр", Patronymic = "Петрович", Age = 35 },
+            new Employee { Id = 3, LastName = "Александров", FirstName = "Пётр", Patronymic = "Иванович", Age = 45 }
+        };
+
         public IActionResult Index() => View();
 
 
         public IActionResult SecondAction() => View("SecondView");
+
+        public IActionResult Employees() => View(employees);
 
     }
 }
