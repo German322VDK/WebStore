f04618b2871db9b82c8480abe11f535c78b0d92e
German322VDK <germean322@gmail.com>
2021-01-23 21:03:48 +1000

    Редактирование сотрудников

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using WebStore.Data;
 using WebStore.Infrastructure.Interfaces;
 using WebStore.Models;
+using WebStore.ViewModels;
 
 namespace WebStore.Controllers
 {
@@ -27,5 +28,57 @@ namespace WebStore.Controllers
                 return View(employee);
             return NotFound();
         }
+
+        #region Edit
+        public IActionResult Edit(int id)
+        {
+            if (id <= 0) return BadRequest();
+
+            var employee = _IEmployeesData.Get((int)id);
+
+            if (employee is null)
+                return NotFound();
+
+            return View(new EmployeeViewModel
+            {
+                Id = employee.Id,
+                FirstName = employee.FirstName,
+                LastName = employee.LastName,
+                Patronymic = employee.Patronymic,
+                Job = employee.Job,
+                Age = employee.Age,
+                Salary = employee.Salary,
+                WorkExperience = employee.WorkExperience
+            });
+        }
+
+        [HttpPost]
+        public IActionResult Edit(EmployeeViewModel model)
+        {
+            if (model is null)
+                throw new ArgumentNullException(nameof(model));
+
+           var employee = new Employee
+            {
+               Id = model.Id,
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                Patronymic = model.Patronymic,
+                Job = model.Job,
+                Age = model.Age,
+                Salary = model.Salary,
+               WorkExperience = model.WorkExperience
+           };
+
+            if (employee.Id == 0)
+                _IEmployeesData.Add(employee);
+            else
+                _IEmployeesData.Update(employee);
+
+            return RedirectToAction("Index");
+        }
+
+        #endregion
+
     }
 }
---
+++ WebStore/ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.ViewModels
+{
+    public class EmployeeViewModel
+    {
+        public int Id { get; init; }
+
+        public string FirstName { get; init; }
+
+        public string LastName { get; init; }
+
+        public string Patronymic { get; init; }
+
+        public string Job { get; init; }
+
+        public int Age { get; init; }
+
+        public int Salary { get; init; }
+
+        public int WorkExperience { get; init; }
+    }
+}
