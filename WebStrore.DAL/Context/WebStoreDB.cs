c8a953f36f8486ae1d1397022685c2ae484f87f9
German322VDK <germean322@gmail.com>
2021-02-03 21:15:36 +1000

    Подмена контекста БД для Identity

--- WebStrore.DAL/Context/WebStoreDB.cs
+++ WebStrore.DAL/Context/WebStoreDB.cs
@@ -1,14 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using WebStore.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using WebStore.Domain.Entities.Identity;
 
 namespace WebStrore.DAL.Context
 {
-    public class WebStoreDB : DbContext
+    public class WebStoreDB : IdentityDbContext<User, Role, string>
     {
         public DbSet<Product> Products { get; set; }
 
