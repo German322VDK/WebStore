74555afdd457d23cadca4cc5cc649b549930c776
German322VDK <germean322@gmail.com>
2021-02-04 17:21:40 +1000

    Вход в систему

--- WebStore/Controllers/AccountController.cs
+++ WebStore/Controllers/AccountController.cs
@@ -23,6 +23,9 @@ namespace WebStore.Controllers
             _Logger = Logger;
         }
 
+
+        #region Register
+
         public IActionResult Register() => View(new RegisterUserViewModel());
 
         [HttpPost, ValidateAntiForgeryToken]
@@ -51,5 +54,41 @@ namespace WebStore.Controllers
 
             return View(Model);
         }
+
+        #endregion
+
+        #region Login
+
+        public IActionResult Login(string ReturnUrl) => View(new LoginViewModel { ReturnUrl = ReturnUrl });
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel Model)
+        {
+            if (!ModelState.IsValid) return View(Model);
+
+            var login_result = await _SignInManager.PasswordSignInAsync(
+                Model.UserName,
+                Model.Password,
+                Model.RememberMe,
+#if DEBUG
+                false
+#else
+                true
+#endif
+                );
+
+            if (login_result.Succeeded)
+            {
+                return LocalRedirect(Model.ReturnUrl ?? "/");
+            }
+
+            ModelState.AddModelError("", "Неверное имя пользователя или пароль");
+
+            return View(Model);
+        }
+
+        #endregion
+
+
     }
 }
