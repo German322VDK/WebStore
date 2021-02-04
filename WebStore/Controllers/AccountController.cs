d6e1ed7a6b08efbc53076b517f503ef4e8e0e026
German322VDK <germean322@gmail.com>
2021-02-04 18:29:46 +1000

    Вывод информации о входе пользователя в аккаунт

--- WebStore/Controllers/AccountController.cs
+++ WebStore/Controllers/AccountController.cs
@@ -44,14 +44,14 @@ namespace WebStore.Controllers
 
             if (registration_result.Succeeded)
             {
-                _Logger.LogInformation("Пользовател {0} успешно зарегестрирован", Model.UserName);
+                _Logger.LogInformation("Пользователь {0} успешно зарегестрирован", Model.UserName);
 
                 await _SignInManager.SignInAsync(user, false);
 
                 return RedirectToAction("Index", "Home");
             }
 
-            _Logger.LogWarning("В прроцессе регистрация пользователя {0} возникли ошибки :( {1}", 
+            _Logger.LogWarning("В процессе регистрации пользователя {0} возникли ошибки :( {1}", 
                 Model.UserName, string.Join(",", registration_result.Errors.Select(e => e.Description)));
 
             foreach (var errors in registration_result.Errors)
@@ -73,6 +73,8 @@ namespace WebStore.Controllers
         {
             if (!ModelState.IsValid) return View(Model);
 
+            _Logger.LogInformation("Вход пользователя {0}", Model.UserName);
+
             var login_result = await _SignInManager.PasswordSignInAsync(
                 Model.UserName,
                 Model.Password,
@@ -86,8 +88,13 @@ namespace WebStore.Controllers
 
             if (login_result.Succeeded)
             {
+                _Logger.LogInformation("Пользователь {0} успешно зашёл", Model.UserName);
+
                 return LocalRedirect(Model.ReturnUrl ?? "/");
             }
+
+            _Logger.LogWarning("В процессе входа пользователя {0} возникли ошибки :( ",
+                Model.UserName);
 
             ModelState.AddModelError("", "Неверное имя пользователя или пароль");
 
