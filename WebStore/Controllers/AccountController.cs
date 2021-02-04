58b641619dff57042f560343a6515ec5b0d68a03
German322VDK <germean322@gmail.com>
2021-02-04 11:58:30 +1000

    Регистация пользователя

--- WebStore/Controllers/AccountController.cs
+++ WebStore/Controllers/AccountController.cs
@@ -1,22 +1,55 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebStore.Domain.Entities.Identity;
+using WebStore.ViewModels;
 
 namespace WebStore.Controllers
 {
     public class AccountController : Controller
     {
         public readonly UserManager<User> _UserManager;
-        public readonly RoleManager<Role> _RoleManager;
+        private readonly SignInManager<User> _SignInManager;
+        private readonly ILogger<AccountController> _Logger;
 
-        public AccountController(UserManager<User> UserManager, RoleManager<Role> RoleManager)
+        public AccountController(UserManager<User> UserManager, SignInManager<User> SignInManager, ILogger<AccountController> Logger)
         {
             _UserManager = UserManager;
-            _RoleManager = RoleManager;
+            _SignInManager = SignInManager;
+            _Logger = Logger;
+        }
+
+        public IActionResult Register() => View(new RegisterUserViewModel());
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterUserViewModel Model)
+        {
+            if (!ModelState.IsValid) return View(Model);
+
+            var user = new User
+            {
+                UserName = Model.UserName
+            };
+
+            var registration_result = await _UserManager.CreateAsync(user, Model.Password);
+
+            if (registration_result.Succeeded)
+            {
+                await _SignInManager.SignInAsync(user, false);
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            foreach (var errors in registration_result.Errors)
+            {
+                ModelState.AddModelError("", errors.Description);
+            }
+
+            return View(Model);
         }
     }
 }
