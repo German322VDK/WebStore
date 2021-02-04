abe359b0da7ab366292da0ba40aeb92e654c2fb7
German322VDK <germean322@gmail.com>
2021-02-04 17:55:03 +1000

    Выход из системы

--- WebStore/Controllers/AccountController.cs
+++ WebStore/Controllers/AccountController.cs
@@ -89,6 +89,18 @@ namespace WebStore.Controllers
 
         #endregion
 
+        public async Task<IActionResult> Logout()
+        {
+            await _SignInManager.SignOutAsync();
 
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult AccessDenied(string ReturnUrl)
+        {
+            ViewBag.ReturnUrl = ReturnUrl;
+
+            return View();
+        }
     }
 }
